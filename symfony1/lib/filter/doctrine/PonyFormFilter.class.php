a50c2cac4dcba5254f8100d014674c8822e8221e
Damien Alexandre <mobman02@gmail.com>
2011-02-15 22:54:01 +0100

    [Symfony1] Add Doctrine classes

---
+++ symfony1/lib/filter/doctrine/PonyFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Pony filter form.
+ *
+ * @package    symfpony
+ * @subpackage filter
+ * @author     Paellas
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class PonyFormFilter extends BasePonyFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ symfony1/lib/form/doctrine/PonyForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Pony form.
+ *
+ * @package    symfpony
+ * @subpackage form
+ * @author     Paellas
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class PonyForm extends BasePonyForm
+{
+  public function configure()
+  {
+  }
+}
