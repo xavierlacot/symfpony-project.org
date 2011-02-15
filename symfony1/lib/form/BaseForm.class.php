d844b27fe6bc7a03955db2f7b29501882ad33815
Xavier Lacot <xavier@lacot.org>
2011-02-15 20:38:25 +0100

    created sf1 project

---
+++ symfony1/lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    symfpony
+ * @subpackage form
+ * @author     Paellas 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
