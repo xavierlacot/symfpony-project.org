9974be0e3a1624ed459fe99850cbc59e65636d22
Xavier Lacot <xavier@lacot.org>
2011-02-16 23:53:41 +0100

    removed useless template, fixed methods visibility;2D

--- symfony1/apps/api/modules/pony/actions/actions.class.php
+++ symfony1/apps/api/modules/pony/actions/actions.class.php
@@ -3,7 +3,7 @@
 /**
  * pony actions.
  * Provide a REST not-ful way of dealing with ponies.
- * 
+ *
  * @package    symfpony
  * @subpackage pony
  * @author     Paellas
@@ -12,57 +12,10 @@
 class ponyActions extends sfActions
 {
   public $model = 'Pony';
-  
-  /**
-   * Retrieves a  collection of Pony
-   * @param   sfWebRequest   $request a request object
-   * @return  string
-   */
-  public function executeIndex(sfWebRequest $request)
-  {
-    $format = $request->getParameter('sf_format');
-    $this->getResponse()->setContentType($format);
-    
-    try
-    {
-      $params = $this->validate($request->getGetParameters(), $this->getIndexValidators());
-    }
-    catch (Exception $e)
-    {
-      $this->getResponse()->setStatusCode(406);
-      $this->getResponse()->setContent(
-              Doctrine_Parser::dump(array('message' => $e->getMessage()), $format)
-      );
-      return sfView::NONE;
-    }
-
-    $this->getResponse()->setContent(
-            $this->query($params)->execute()->exportTo($format)
-    );
-
-    return sfView::NONE;
-  }
 
   /**
    * Retrieve a single Pony from the collection
-   * @param sfWebRequest $request
-   */
-  public function executeShow(sfWebRequest $request)
-  {
-    $pony   = $this->getRoute()->getObject();
-    $format = $request->getParameter('sf_format');
-    
-    $this->getResponse()->setContentType($format);
-    
-    $this->getResponse()->setContent(
-            Doctrine_Parser::dump($pony->toArray(true, true), $format)
-    );
-
-    return sfView::NONE;
-  }
-
-  /**
-   * Retrieve a single Pony from the collection
+   *
    * @param sfWebRequest $request
    */
   public function executeDelete(sfWebRequest $request)
@@ -75,14 +28,14 @@ class ponyActions extends sfActions
     if ($pony->delete())
     {
       $this->getResponse()->setContent(
-              Doctrine_Parser::dump(array('message' => 'Goodbye my little pony, hope you are in a better place now :('), $format)
+        Doctrine_Parser::dump(array('message' => 'Goodbye my little pony, hope you are in a better place now :('), $format)
       );
     }
     else
     {
       $this->getResponse()->setStatusCode(500);
       $this->getResponse()->setContent(
-              Doctrine_Parser::dump(array('message' => 'You CANT kill mister PONY HAHAHAHAHA!'), $format)
+        Doctrine_Parser::dump(array('message' => 'You CANT kill mister PONY HAHAHAHAHA!'), $format)
       );
     }
 
@@ -90,16 +43,85 @@ class ponyActions extends sfActions
   }
 
   /**
+   * Retrieves a  collection of Pony
+   *
+   * @param   sfWebRequest   $request a request object
+   * @return  string
+   */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $format = $request->getParameter('sf_format');
+    $this->getResponse()->setContentType($format);
+
+    try
+    {
+      $params = $this->validate($request->getGetParameters(), $this->getIndexValidators());
+    }
+    catch (Exception $e)
+    {
+      $this->getResponse()->setStatusCode(406);
+      $this->getResponse()->setContent(
+        Doctrine_Parser::dump(array('message' => $e->getMessage()), $format)
+      );
+      return sfView::NONE;
+    }
+
+    $this->getResponse()->setContent(
+      $this->query($params)->execute()->exportTo($format)
+    );
+
+    return sfView::NONE;
+  }
+
+  /**
+   * Retrieve a single Pony from the collection
+   *
+   * @param sfWebRequest $request
+   */
+  public function executeShow(sfWebRequest $request)
+  {
+    $pony   = $this->getRoute()->getObject();
+    $format = $request->getParameter('sf_format');
+
+    $this->getResponse()->setContentType($format);
+
+    $this->getResponse()->setContent(
+     Doctrine_Parser::dump($pony->toArray(true, true), $format)
+    );
+
+    return sfView::NONE;
+  }
+
+  /**
    * Returns the list of validators for a get request.
+   *
    * @return  array  an array of validators
    */
-  public function getIndexValidators()
+  protected function getIndexValidators()
   {
   	$validators = array();
     $validators['id'] = new sfValidatorInteger(array('required' => false));
     $validators['name'] = new sfValidatorString(array('max_length' => 255, 'required' => false));
-    
+
     return $validators;
+  }
+
+  /**
+   * Build the Doctrine_Query with the params from the request
+   *
+   * @param array $params
+   * @return Doctrine_Query
+   */
+  protected function query($params)
+  {
+    $q = Doctrine_Query::create()->from($this->model.' '.$this->model);
+
+    foreach ($params as $name => $value)
+    {
+      $q->andWhere($this->model.'.'.$name.' = ?', $value);
+    }
+
+    return $q;
   }
 
   /**
@@ -109,7 +131,7 @@ class ponyActions extends sfActions
    * @param array   $validators  An array of validators
    * @throw sfException
    */
-  public function validate($params, $validators, $prefix = '')
+  protected function validate($params, $validators, $prefix = '')
   {
     $clean_params = array();
 
@@ -125,22 +147,5 @@ class ponyActions extends sfActions
       }
     }
     return $clean_params;
-  }
-
-  /**
-   * Build the Doctrine_Query with the params from the request
-   * @param array $params
-   * @return Doctrine_Query
-   */
-  public function query($params)
-  {
-    $q = Doctrine_Query::create()->from($this->model.' '.$this->model);
-
-    foreach ($params as $name => $value)
-    {
-      $q->andWhere($this->model.'.'.$name.' = ?', $value);
-    }
-    
-    return $q;
   }
 }
