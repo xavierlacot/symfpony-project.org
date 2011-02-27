1e4c42bdfd643f36ba4457b4bad08d7014b0499c
Xavier Lacot <xavier@lacot.org>
2011-02-27 20:55:19 +0100

    added create action, with validation

--- Symfony2/src/CleverAge/SymfponyBundle/Controller/DefaultController.php
+++ Symfony2/src/CleverAge/SymfponyBundle/Controller/DefaultController.php
@@ -12,11 +12,83 @@ use CleverAge\SymfponyBundle\Entity\Pony;
 class DefaultController extends Controller
 {
     /**
+     * Creates a new Pony
+     *
+     * @param string $slug
+     * @param string $_format
+     */
+    public function createAction($_format)
+    {
+        // create the serializer
+        $serializer = $this->getSerializer($_format);
+
+        // build the Pony from a decoded array
+        $pony = $serializer->denormalizeObject(
+            $serializer->decode($this->get('request')->getContent(), $_format),
+            'CleverAge\SymfponyBundle\Entity\Pony',
+            $_format
+        );
+
+        if ($pony)
+        {
+            // validate the pony
+            $validator = $this->get('validator');
+
+            if (0 === count($validator->validate($pony)))
+            {
+                // this is a valid Pony instance, persist it
+                $em = $this->getEntityManager();
+                $em->persist($pony);
+                $em->flush();
+
+                // redirect to this Pony
+                $router = $this->get('router');
+                $response = new \Symfony\Component\HttpFoundation\RedirectResponse(
+                    $router->generate('pony_show', array(
+                        'slug' => $pony->getSlug(),
+                        '_format' => $_format
+                    ))
+                );
+                return $response;
+            }
+        }
+
+        // something went wrong, cry
+        $response = new Response('invalid pony!', 406);
+        $response->setPublic();
+        $response->setSharedMaxAge(120);
+        return $response;
+    }
+
+    /**
+     * Kills a pony
+     *
+     * @param string $slug
+     * @param string $_format
+     */
+    public function deleteAction($slug, $_format)
+    {
+        // fetch a Pony with a Request param, here it's the slug too
+        $pony = $this->getEntityManager()->getRepository('CleverAgeSymfponyBundle:Pony')->findOneBySlug($slug);
+
+        if ($pony)
+        {
+            $this->getEntityManager()->remove($pony);
+            $this->getEntityManager()->flush();
+            return new Response(null); // Send only 200 response.
+        }
+        else
+        {
+            throw new NotFoundHttpException(); // Return a nice 404 response
+        }
+    }
+
+    /**
      * @return \Doctrine\ORM\EntityManager
      */
     private function getEntityManager()
     {
-      return $this->get('doctrine.orm.entity_manager');
+        return $this->get('doctrine.orm.entity_manager');
     }
 
     public function indexAction($_format)
@@ -42,8 +114,8 @@ class DefaultController extends Controller
     public function showAction(Pony $pony, $_format)
     {
         $response = new Response();
-        $request  = $this->get("request");
-        
+        $request = $this->get("request");
+
         $response->setPublic();
         $response->setSharedMaxAge(120);
         $response->setETag(\md5(\serialize($pony))); // Should be a method in Pony
@@ -52,53 +124,36 @@ class DefaultController extends Controller
         {
             // return the 304 Response immediately
             return $response;
-        } 
+        }
         else
         {
-            $response->setContent( $this->getSerializer($_format)->encode($pony, $_format) );
+            $response->setContent(
+                $this->getSerializer($_format)->encode($pony, $_format)
+            );
             return $response;
-        }
-    }
-
-    /**
-     * The other way is to manualy fetch a Pony with a Request param, here it's the slug too.
-     * 
-     * @param string $slug
-     * @param string $_format
-     */
-    public function deleteAction($slug, $_format)
-    {
-        $pony = $this->getEntityManager()->getRepository('CleverAgeSymfponyBundle:Pony')->findOneBySlug($slug);
-
-        if ($pony)
-        {
-          $this->getEntityManager()->remove($pony);
-          $this->getEntityManager()->flush();
-          return new Response(null); // Send only 200 response.
-        }
-        else
-        {
-          throw new NotFoundHttpException(); // Return a nice 404 response
         }
     }
 
     private function getSerializer($format)
     {
-        // create serializer
-        $serializer = new Serializer\Serializer();
-        $serializer->addNormalizer(new Serializer\Normalizer\CustomNormalizer());
-
-        if ('json' == $format)
+        if (!isset($this->serializer))
         {
-            // add json encoder
-            $serializer->setEncoder('json', new Serializer\Encoder\JsonEncoder());
-        }
-        elseif ('xml' == $format)
-        {
-            // add xml encoder
-            $serializer->setEncoder('xml', new Serializer\Encoder\XmlEncoder());
+            // create serializer
+            $this->serializer = new Serializer\Serializer();
+            $this->serializer->addNormalizer(new Serializer\Normalizer\CustomNormalizer());
+
+            if ('json' == $format)
+            {
+                // add json encoder
+                $this->serializer->setEncoder('json', new Serializer\Encoder\JsonEncoder());
+            }
+            elseif ('xml' == $format)
+            {
+                // add xml encoder
+                $this->serializer->setEncoder('xml', new Serializer\Encoder\XmlEncoder());
+            }
         }
 
-        return $serializer;
+        return $this->serializer;
     }
 }
